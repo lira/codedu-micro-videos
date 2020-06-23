f291dc517c200941f1b1c2857f8cc6a22eea1545
Fernando Lira <local@fernand.us>
2020-06-22 23:56:39 -0300

    finalizando aula de categorias e criação de gêneros

---
+++ database/seeds/CategoriesSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+use App\Models\Category;
+use Illuminate\Database\Seeder;
+
+class CategoriesSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        factory(Category::class, 100)->create();
+    }
+}
---
+++ database/seeds/GenresSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+use App\Models\Genre;
+use Illuminate\Database\Seeder;
+
+class GenresSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        factory(Genre::class, 10)->create();
+    }
+}
