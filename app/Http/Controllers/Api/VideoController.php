70ebaa6d2070fcd45e7358438598ab63fe0422a7
Fernando Lira <local@fernand.us>
2020-07-21 00:25:52 -0300

    criados relacionamentos

--- app/Http/Controllers/Api/VideoController.php
+++ app/Http/Controllers/Api/VideoController.php
@@ -18,7 +18,31 @@ class VideoController extends BasicCrudController
             'opened' => 'boolean',
             'rating' => 'required|in:' . implode(',', Video::RATING_LIST),
             'duration' => 'required|integer',
+            'categories_id' => 'required|array|exists:categories,id',
+            'genres_id' => 'required|array|exists:genres,id',
         ];
+    }
+
+    public function store(Request $request)
+    {
+        $validatedData = $this->validate($request, $this->rulesStore());
+        /** @var Video $object */
+        $object = $this->model()::create($validatedData);
+        $object->categories()->sync($request->get('categories_id'));
+        $object->genres()->sync($request->get('genres_id'));
+        $object->refresh();
+        return $object;
+    }
+
+    public function update(Request $request, $id)
+    {
+        $validatedData = $this->validate($request, $this->rulesUpdate());
+        $object = $this->findOrFail($id);
+        $object->update($validatedData);
+        $object->categories()->sync($request->get('categories_id'));
+        $object->genres()->sync($request->get('genres_id'));
+        $object->refresh();
+        return $object;
     }
 
     protected function model()
