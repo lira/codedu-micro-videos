7b17bec26b6a3ec96bf3659528c449a77a4f2d30
Fernando Lira <local@fernand.us>
2020-07-20 23:51:49 -0300

    finalizando testes do controller de video

---
+++ tests/Feature/Http/Controllers/Api/VideoControllerTest.php
@@ -0,0 +1,188 @@
+<?php
+
+namespace Tests\Feature\Http\Controllers\Api;
+
+use App\Models\Video;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Tests\TestCase;
+use Tests\Traits\TestSaves;
+use Tests\Traits\TestValidations;
+
+class VideoControllerTest extends TestCase
+{
+    use DatabaseMigrations, TestValidations, TestSaves;
+
+    /**
+     * @var Video
+     */
+    private $video;
+
+    private $sendData;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->video = factory(Video::class)->create(['opened' => false]);
+        $this->sendData = [
+            'title' => 'title',
+            'description' => 'description',
+            'year_launched' => 2010,
+            'rating' => Video::RATING_LIST[0],
+            'duration' => 90,
+        ];
+    }
+
+    public function testIndex()
+    {
+        $response = $this->get(route('api.videos.index'));
+
+        $response
+            ->assertStatus(200)
+            ->assertJson([$this->video->toArray()])
+        ;
+    }
+
+    public function testInvalidationRequired()
+    {
+        $data = [
+            'title' => '',
+            'description' => '',
+            'year_launched' => '',
+            'rating' => '',
+            'duration' => '',
+        ];
+
+        $this->assertInvalidationInStoreAction($data, 'required');
+        $this->assertInvalidationInUpdateAction($data, 'required');
+    }
+
+    public function testInvalidationMax()
+    {
+        $data = [
+            'title' => str_repeat('a', 256),
+        ];
+
+        $this->assertInvalidationInStoreAction($data, 'max.string', ['max' => 255]);
+        $this->assertInvalidationInUpdateAction($data, 'max.string', ['max' => 255]);
+    }
+
+    public function testInvalidationInteger()
+    {
+        $data = [
+            'year_launched' => 'a',
+        ];
+
+        $this->assertInvalidationInStoreAction($data, 'date_format', ['format' => 'Y']);
+        $this->assertInvalidationInUpdateAction($data, 'date_format', ['format' => 'Y']);
+    }
+
+    public function testInvalidationOpenedField()
+    {
+        $data = [
+            'opened' => 's',
+        ];
+
+        $this->assertInvalidationInStoreAction($data, 'boolean');
+        $this->assertInvalidationInUpdateAction($data, 'boolean');
+    }
+
+    public function testInvalidationRatingField()
+    {
+        $data = [
+            'rating' => 0,
+        ];
+
+        $this->assertInvalidationInStoreAction($data, 'in');
+        $this->assertInvalidationInUpdateAction($data, 'in');
+    }
+
+    public function testShow()
+    {
+        $response = $this->get(route('api.videos.show', ['video' => $this->video->id]));
+
+        $response
+            ->assertStatus(200)
+            ->assertJson($this->video->toArray())
+        ;
+    }
+
+    public function testSave()
+    {
+        $data = [
+            [
+                'send_data' => $this->sendData,
+                'test_data' => $this->sendData + ['opened' => false],
+            ],
+            [
+                'send_data' => $this->sendData + ['opened' => true],
+                'test_data' => $this->sendData + ['opened' => true],
+            ],
+            [
+                'send_data' => $this->sendData + ['rating' => Video::RATING_LIST[1]],
+                'test_data' => $this->sendData + ['rating' => Video::RATING_LIST[1]],
+            ],
+        ];
+
+        foreach ($data as $value) {
+            $response = $this->assertStore(
+                $value['send_data'],
+                $value['test_data'] + ['deleted_at' => null]
+            );
+            $response->assertJsonStructure(
+                [
+                    'created_at', 'updated_at'
+                ]
+            );
+            $response = $this->assertUpdate(
+                $value['send_data'],
+                $value['test_data'] + ['deleted_at' => null]
+            );
+            $response->assertJsonStructure(
+                [
+                    'created_at', 'updated_at'
+                ]
+            );
+        }
+    }
+
+    public function testDelete()
+    {
+        /** @var Video $video */
+        $video = factory(Video::class)->create();
+
+        $id = $video->id;
+
+        $response = $this->json(
+            'DELETE',
+            route('api.videos.destroy', ['video' => $id])
+        );
+
+        $response->assertStatus(204);
+
+        $response = $this->json(
+            'GET',
+            route('api.videos.show', ['video' => $id])
+        );
+
+        $response->assertStatus(404);
+
+
+        $this->assertNull(Video::find($id));
+        $this->assertNotNull(Video::withTrashed()->find($id));
+    }
+
+    protected function routeStore()
+    {
+        return route('api.videos.store');
+    }
+
+    protected function routeUpdate()
+    {
+        return route('api.videos.update', ['video' => $this->video->id]);
+    }
+
+    protected function model()
+    {
+        return Video::class;
+    }
+}
