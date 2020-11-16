ca26aa4ecf8de28a5d10f95924654c1d3888d0cb
Fernando Lira <local@fernand.us>
2020-11-16 19:40:06 -0300

    atualizando laravel

--- app/Models/Genre.php
+++ app/Models/Genre.php
@@ -6,6 +6,31 @@ use App\Models\Traits\Uuid;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
+/**
+ * App\Models\Genre
+ *
+ * @property string $id
+ * @property string $name
+ * @property bool $is_active
+ * @property \Illuminate\Support\Carbon|null $deleted_at
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property-read \Illuminate\Database\Eloquent\Collection|\App\Models\Category[] $categories
+ * @property-read int|null $categories_count
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Genre newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Genre newQuery()
+ * @method static \Illuminate\Database\Query\Builder|\App\Models\Genre onlyTrashed()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Genre query()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Genre whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Genre whereDeletedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Genre whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Genre whereIsActive($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Genre whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Genre whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\Models\Genre withTrashed()
+ * @method static \Illuminate\Database\Query\Builder|\App\Models\Genre withoutTrashed()
+ * @mixin \Eloquent
+ */
 class Genre extends Model
 {
     use SoftDeletes, Uuid;
